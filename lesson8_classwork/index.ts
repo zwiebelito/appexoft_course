2cab56a78fd9c004fbbd017edf5aa7799db5c351
zwiebelito <tsybulenkoihor@gmail.com>
2023-12-01 18:31:45 +0200

    final commit

---
+++ lesson8_classwork/index.ts
@@ -0,0 +1,22 @@
+/*
+function celciusToFahrenheit(celsius: number): number {
+    return (celsius*9/5)+32;
+}
+
+const celsiusTemperature: number = 30;
+const fahrenheit = celciusToFahrenheit(celsiusTemperature);
+console.log(`${celsiusTemperature} C is equal to ${fahrenheit}F`)*/
+
+
+/*
+enum DaysOfWeek {
+    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+}
+
+function isWorkDay(day: DaysOfWeek):boolean {
+    return day >= DaysOfWeek.Monday && day <= DaysOfWeek.Friday
+}
+
+const today = DaysOfWeek.Sunday;
+console.log(`Is today a working day? ${isWorkDay(today) ? 'yes': 'no'}`)*/
+
